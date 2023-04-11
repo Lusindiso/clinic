1781deb73514d848b4429c45345ac5665a4514a3
Lusindiso Ntanjana <lusindisontanjana@gmail.com>
2023-04-11 16:01:10 +0200

    Create medical histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,9 @@ CREATE TABLE patients (
   date_of_birth DATE ,
 );
 
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP ,
+  patient_id INT FOREIGN KEY REFERENCES patients(id),
+  status VARCHAR(50),
+);
